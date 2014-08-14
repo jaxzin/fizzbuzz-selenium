d0613ea67630e072fac9aadf0765ae7c384597cf
Charlie Cantoni <charles.cantoni@gmail.com>
2014-08-13 21:49:10 -0400

    Importing os module

--- navigate_to_nfl_scoreboard.py
+++ navigate_to_nfl_scoreboard.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-import unittest, time, re
+import unittest, time, re, os
 
 class NavigateToNflScoreboard(unittest.TestCase):
     def setUp(self):
