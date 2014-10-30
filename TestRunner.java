7e68cb5a7e33fa62d80e312909beb0dda910c270
VenkataAdidamu <ram.adidamu@gmail.com>
2014-10-29 21:20:26 -0400

    Update TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -11,3 +11,18 @@ public class TestRunner {
       System.out.println(result.wasSuccessful());
    }
 } 
+
+/*
+This is a test created to check the page title of Scores in the espn website.
+To Execute these tests and replicate the result
+Create a work space for Juint /TestNg{Depends on which testing framework you prefer}
+
+Using Test NG
+C:\TestNG_WorkSpace/javac  CheckPageTitleNflScores.java
+C:\java -cp "C:\TestNG_WORKSPACE" org.testng.TestNG testng.xml
+ // testng.xml is the maven file we created to test include the test case.
+
+Using Junit
+C:\Junit_WorkSpace/javac  CheckPageTitleNflScores.java
+C:\Junit_WORKSPACE\java TestRunner
+*/
