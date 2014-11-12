74bec040f0cb498a27dede1b9837a8f91f0a0c29
unknown <fdkzb@fdkzb-m92.hq.bn-corp.com>
2014-11-12 17:45:35 -0500

    Modifi Smoke test Script

--- src/test/java/script/SmokeTest.java
+++ src/test/java/script/SmokeTest.java
@@ -2,8 +2,11 @@ package script;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -24,13 +27,19 @@ public class SmokeTest {
 		
 		// Windows maximize
 		driver.manage().window().maximize();
+		//driver.manage().deleteAllCookies();
 	}
 	
 	@Test
-	public void main() {
+	public void main() throws InterruptedException {
 		
 		// Navigate to http://espn.go.com
 		driver.navigate().to("http://espn.go.com");
+		
+		WebElement nfl= driver.findElement(By.name("&lpos=sitenavdefault&lid=sitenav_nfl"));
+		new Actions(driver).moveToElement(nfl).perform();
+		driver.findElement(By.name("&lpos=sitenavdefault&lid=nfl_scores")).click();
+		
 		
 		//Validate current page title is not "NFL Scoreboard".
 		Assert.assertFalse(driver.getTitle().equals("NFL Scoreboard"));
