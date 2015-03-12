e117036fec469de1e4e0646c1c34cbf953c2469d
Matt Thomas <matt@betweenbrain.com>
2015-03-12 11:26:07 -0400

    Add hovering of NFL parent menu item and then finds and clicks the Scores child menu item

--- test.py
+++ test.py
@@ -4,17 +4,27 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Import actions (e.g. hovering)
+from selenium.webdriver.common.action_chains import ActionChains
+
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
 # Navigate to http://espn.go.com/
 driver.get("http://espn.go.com/")
 
-# Find the NFL > Scores link
-inputElement = driver.find_element_by_name("&lpos=sitenavdefault&lid=sitenav_nfl")
+# Find the NFL parent menu item
+parentElement = driver.find_element_by_name("&lpos=sitenavdefault&lid=sitenav_nfl")
+
+# Hover over the NFL parent menu item
+hov = ActionChains(driver).move_to_element(parentElement)
+hov.perform()
+
+# Find the Scores child menu item
+childElement = driver.find_element_by_name("&lpos=sitenavdefault&lid=nfl_scores")
 
 # Click the found link
-inputElement.click()
+childElement.click()
 
 try:
     # we have to wait for the page to refresh, the last thing that seems to be updated is the title
